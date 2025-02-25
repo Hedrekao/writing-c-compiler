34b306abc9afb5d7e14ac0bfddb3732403d28103
hedrekao <82450784+Hedrekao@users.noreply.github.com>
2025-02-26 00:06:04 +0100

    basic recursive descent parser

---
+++ c_compiler/lib/parser.ex
@@ -0,0 +1,65 @@
+defmodule Parser do
+  defp expect_token([], expected_type), do: raise("Expected token :#{expected_type}, got EOF")
+
+  defp expect_token([{token_type, value} | rest], expected_type) do
+    if token_type == expected_type do
+      rest
+    else
+      raise "Expected token :#{expected_type}, got token :#{token_type} \"#{value}\""
+    end
+  end
+
+  defp peek_token([], _), do: false
+
+  defp peek_token([{token_type, _} | _], expected_type) do
+    token_type == expected_type
+  end
+
+  def parse_program(tokens) do
+    try do
+      {function, remaining_tokens} = parse_function_definition(tokens)
+
+      if remaining_tokens == [] do
+        {:ok, Ast.Program.new([function])}
+      else
+        {:error, "Expected EOF, got #{inspect(remaining_tokens)}"}
+      end
+    rescue
+      e in RuntimeError -> {:error, Exception.message(e)}
+    end
+  end
+
+  defp parse_function_definition(tokens) do
+    tokens = expect_token(tokens, :int)
+    peek_token(tokens, :identifier) || raise "Expected function name, got #{inspect(hd(tokens))}"
+    {function_name, tokens} = parse_expression(tokens)
+    tokens = expect_token(tokens, :open_parenthesis)
+    tokens = expect_token(tokens, :void)
+    tokens = expect_token(tokens, :close_parenthesis)
+    tokens = expect_token(tokens, :open_brace)
+
+    {function_body, tokens} = parse_statement(tokens)
+
+    tokens = expect_token(tokens, :close_brace)
+
+    {Ast.FunctionDefinition.new(function_name, function_body), tokens}
+  end
+
+  defp parse_statement(tokens) do
+    tokens = expect_token(tokens, :return)
+    {return_expression, tokens} = parse_expression(tokens)
+    tokens = expect_token(tokens, :semicolon)
+
+    {Ast.Statement.return(return_expression), tokens}
+  end
+
+  defp parse_expression([]), do: raise("Expected expression, got EOF")
+
+  defp parse_expression([{:identifier, value} | rest]) do
+    {Ast.Expression.identifier(value), rest}
+  end
+
+  defp parse_expression([{:constant, value} | rest]) do
+    {Ast.Expression.constant(value), rest}
+  end
+end
